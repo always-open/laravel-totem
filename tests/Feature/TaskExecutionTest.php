837273b5a821106fbea743383265b71aac40fd82
Quentin Schmick <qschmick@gmail.com>
2025-07-10 11:42:03 -0400

    Laravel 12 updates (#402)
    * Updating composer deps for laravel 12; Removed old versions from readme; Updating workflows; updated phpunit version

--- tests/Feature/TaskExecutionTest.php
+++ tests/Feature/TaskExecutionTest.php
@@ -13,8 +13,7 @@ use Studio\Totem\Tests\TestCase;
 
 class TaskExecutionTest extends TestCase
 {
-    /** @test */
-    public function it_runs_a_scheduled_task()
+    public function test_it_runs_a_scheduled_task()
     {
         $task = Task::factory()->create();
 
@@ -36,8 +35,7 @@ class TaskExecutionTest extends TestCase
         Event::assertDispatched(Executed::class);
     }
 
-    /** @test */
-    public function it_executes_a_scheduled_task()
+    public function test_it_executes_a_scheduled_task()
     {
         $task = Task::factory()->create();
 
